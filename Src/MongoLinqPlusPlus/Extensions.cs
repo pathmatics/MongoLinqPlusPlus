82eaec961d058e823d3918fafdb0e8436f379f55
Dongchen Xu <dongchen.xu@sensortower.com>
2023-04-05 16:26:16 -0700

    update hextobyte

--- Src/MongoLinqPlusPlus/Extensions.cs
+++ Src/MongoLinqPlusPlus/Extensions.cs
@@ -92,10 +92,12 @@ namespace MongoLinqPlusPlus
 
         public static byte[] StringToByteArray(string hex)
         {
-            return Enumerable.Range(0, hex.Length)
-                .Where(x => x % 2 == 0)
-                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                .ToArray();
+            //could use hexStringToBytes when we upgrade to Net 5+
+            var numberChars = hex.Length;
+            var bytes = new byte[numberChars / 2];
+            for (var i = 0; i < numberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
         }
     }
 }
